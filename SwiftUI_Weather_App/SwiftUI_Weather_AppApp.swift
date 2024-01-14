5396babec6968b232c75829cd5c953d708ae0cf9
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-01-14 21:33:17 +0300

    Initial Commit

---
+++ SwiftUI_Weather_App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI_Weather_App
+//
+//  Created by MIKHAIL ZHACHKO on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
